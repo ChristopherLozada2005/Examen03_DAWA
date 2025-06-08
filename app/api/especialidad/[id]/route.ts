69c1a1fbb20e6dc37605f2ef0c2c7e7cccf35037
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-06-07 22:02:05 -0500

    Deploy try - 1

--- app/api/especialidad/[id]/route.ts
+++ app/api/especialidad/[id]/route.ts
@@ -1,20 +1,18 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
 
-export async function PUT(req: Request, context: { params: { id: string } }) {
-    const { id } = context.params;
-    const data = await req.json();
-    const especialidad = await prisma.especialidad.update({
-        where: { CodEspec: parseInt(id) },
-        data,
-    });
-    return NextResponse.json(especialidad);
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const data = await req.json();
+  const especialidad = await prisma.especialidad.update({
+    where: { CodEspec: parseInt(params.id) },
+    data,
+  });
+  return NextResponse.json(especialidad);
 }
 
-export async function DELETE(req: Request, context: { params: { id: string } }) {
-    const { id } = context.params;
-    await prisma.especialidad.delete({
-        where: { CodEspec: parseInt(id) }
-    });
-    return NextResponse.json({ message: "Especialidad eliminada" });
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  await prisma.especialidad.delete({
+    where: { CodEspec: parseInt(params.id) },
+  });
+  return NextResponse.json({ message: 'Especialidad eliminada' });
 }
--- app/api/medicamento/route.ts
+++ app/api/medicamento/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
-import  prisma  from "@/lib/prisma";
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
 
-export async function POST( req: Request) {
-    const data = await req.json();
-    data.fechaFabricacion = new Date(data.fechaFabricacion);
-    data.fechaVencimiento = new Date(data.fechaVencimiento);
-    const medicamento = await prisma.medicamento.create({ data });
-    return NextResponse.json(medicamento);
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  data.fechaFabricacion = new Date(data.fechaFabricacion);
+  data.fechaVencimiento = new Date(data.fechaVencimiento);
+  const medicamento = await prisma.medicamento.create({ data });
+  return NextResponse.json(medicamento);
 }
 
 export async function GET() {
-    const medicamentos = await prisma.medicamento.findMany({
-        include: { especialidad: true }
-    });
-    return NextResponse.json(medicamentos);
+  const medicamentos = await prisma.medicamento.findMany({
+    include: { especialidad: true },
+  });
+  return NextResponse.json(medicamentos);
 }
