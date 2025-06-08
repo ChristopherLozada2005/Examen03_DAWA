269ec2be4767415d5b09e06c120d2487ac499a56
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-06-07 22:13:12 -0500

    Deploy try - 4

--- app/api/especialidad/[id]/route.ts
+++ app/api/especialidad/[id]/route.ts
@@ -1,18 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop(); // obtener "id" de la URL
   const data = await req.json();
+
   const especialidad = await prisma.especialidad.update({
-    where: { CodEspec: parseInt(context.params.id) },
+    where: { CodEspec: parseInt(id!) },
     data,
   });
+
   return NextResponse.json(especialidad);
 }
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop();
+
   await prisma.especialidad.delete({
-    where: { CodEspec: parseInt(context.params.id) },
+    where: { CodEspec: parseInt(id!) },
   });
+
   return NextResponse.json({ message: 'Especialidad eliminada' });
 }
--- app/api/medicamento/[id]/route.ts
+++ app/api/medicamento/[id]/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop();
+
   const medicamento = await prisma.medicamento.findUnique({
-    where: { CodMedicamento: parseInt(context.params.id) },
+    where: { CodMedicamento: parseInt(id!) },
   });
 
   if (!medicamento) {
@@ -13,21 +15,27 @@ export async function GET(req: NextRequest, context: { params: { id: string } })
   return NextResponse.json(medicamento);
 }
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop();
   const data = await req.json();
+
   data.fechaFabricacion = new Date(data.fechaFabricacion);
   data.fechaVencimiento = new Date(data.fechaVencimiento);
 
   const medicamento = await prisma.medicamento.update({
-    where: { CodMedicamento: parseInt(context.params.id) },
+    where: { CodMedicamento: parseInt(id!) },
     data,
   });
+
   return NextResponse.json(medicamento);
 }
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop();
+
   await prisma.medicamento.delete({
-    where: { CodMedicamento: parseInt(context.params.id) },
+    where: { CodMedicamento: parseInt(id!) },
   });
+
   return NextResponse.json({ message: 'Medicamento eliminado' });
 }
